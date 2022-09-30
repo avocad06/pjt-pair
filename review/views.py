e0da1efb688337a0b9c48dcec3968f9afeb07799
kyominkim1074 <kyominkim1074@gmail.com>
2022-09-30 11:47:54 +0900

    프로젝트 설계 완료

--- review/views.py
+++ review/views.py
@@ -1,3 +1,54 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from review.models import Review
 
 # Create your views here.
+def index(request):
+    review = Review.objects.all()
+    context = {
+        "reviews": review,
+    }
+    return render(request, "review/index.html", context)
+
+
+def new(request):
+    return render(request, "review/new.html")
+
+
+def create(request):
+    title = request.GET.get("title")
+    content = request.GET.get("content")
+    Review.objects.create(title=title, content=content)
+    return redirect("review:index")
+
+
+def detail(request, pk):
+    review = Review.objects.get(pk=pk)
+    context = {
+        "review": review,
+    }
+    return render(request, "review/detail.html", context)
+
+
+def edit(request, pk):
+    review = Review.objects.get(pk=pk)
+    context = {
+        "review": review,
+    }
+    return render(request, "review/edit.html", context)
+
+
+def update(request, pk):
+    review = Review.objects.get(pk=pk)
+    title = request.GET.get("title")
+    content = request.GET.get("content")
+
+    review.title = title
+    review.content = content
+    review.save()
+    return redirect("review:detail", review.pk)
+
+
+def delete(request, pk):
+    review = Review.objects.get(pk=pk)
+    review.delete()
+    return redirect("review:index")
